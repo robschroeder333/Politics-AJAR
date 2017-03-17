821151a813d9f14f8134594bcbabbfbf000b6cdc
Robert Schroeder <robschroeder333@users.noreply.github.com>
2017-03-17 19:09:36 -0400

    test file update and ready

--- test.js
+++ test.js
@@ -1,9 +1,33 @@
 const axios = require('axios');
 
-axios.get(
-  "https://api.propublica.org/congress/v1/115/senate/members.json",{
-  headers: {
-    'X-API-Key': 'apikeygoeshere....'
-  }
-}).then((response) => console.log(response.data.results));
 
+
+
+/////////////////////////
+//DONT RUN BOTH AT ONCE//
+/////////////////////////
+
+// maplight method
+var session = 109;
+var jurisdiction = 'us';
+var apikey = 'INSERT API';
+
+axios.get(
+  `http://maplight.org/services_open_api/map.bill_list_v1.json/?apikey=${apikey}&jurisdiction=${jurisdiction}&session=${session}`,{
+})
+.then((response) =>
+  console.log(response)
+)
+.catch(err => console.log(err));
+
+//pro publica method
+axios.get(
+  `https://api.propublica.org/congress/v1/115/senate/members.json`,{
+  headers: {
+    'X-API-Key': 'INSERT API'
+  }
+})
+.then((response) =>
+  console.log(response)
+)
+.catch(err => console.log(err));
