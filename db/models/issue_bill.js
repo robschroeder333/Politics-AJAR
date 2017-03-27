650cd46c16ce1e70b140ce57baafc6dccfc4f114
Jonathan Fuchs <jgfuchs@gmail.com>
2017-03-27 12:05:42 -0400

    feat(models): for/against functionality added for organizations on bills

--- db/models/issue_bill.js
+++ db/models/issue_bill.js
@@ -6,11 +6,9 @@ const Sequelize = require('sequelize');
 const db = require('../_db.js');
 
 const IssueBills = db.define('issue_bills', {
-	// id: {
- //    	type: Sequelize.INTEGER,
- //    	primaryKey: true,
- //    	autoIncrement: true // Automatically gets converted to SERIAL for postgres
- //  	}
+	forOrAgainst: {
+    	type: Sequelize.ENUM('for', 'against'),
+  	}
 });
 
 module.exports = IssueBills;
--- db/seed.js
+++ db/seed.js
@@ -8,21 +8,21 @@ const Bill = db.model('bills');
 const Cat = db.model('cats');
 
 //Api data
-const allData = require('../test');
+// const allData = require('../test');
 
 //Seed functions
-const seedIssues = (issues) => db.Promise.map(issues,
-  (issue) => {
-    const formattedIssue = {
-      catCode: issue.Catcode,
-      name: issue.Catname
-    };
-    return db.model('issues').create(formattedIssue);
-});
-// const seedIssues = () => db.Promise.map([
-// 	{catCode: 'J6100', name: 'Anti-Guns'},
-// 	{catCode: 'J6200', name: 'Pro-Guns'}
-// ], issue => db.model('issues').create(issue));
+// const seedIssues = (issues) => db.Promise.map(issues,
+//   (issue) => {
+//     const formattedIssue = {
+//       catCode: issue.Catcode,
+//       name: issue.Catname
+//     };
+//     return db.model('issues').create(formattedIssue);
+// });
+const seedIssues = () => db.Promise.map([
+	{catCode: 'J6100', name: 'Anti-Guns'},
+	{catCode: 'J6200', name: 'Pro-Guns'}
+], issue => db.model('issues').create(issue));
 
 const seedCats = () => db.Promise.map([
   {name: 'Gun Control'}
@@ -30,65 +30,63 @@ const seedCats = () => db.Promise.map([
 
 let billDupeCounter = 0;
 
-const seedBills = (billsArray) => db.Promise.map(billsArray,
-  (bill) => {
-    const formattedBill = {
-      prefix: bill.prefix,
-      number: bill.number.toString(),
-      name: bill.name,
-      year: bill.year,
-      session: bill.session
-    };
-    return db.model('bills').create(formattedBill).catch(() => billDupeCounter++)
-})
+// const seedBills = (billsArray) => db.Promise.map(billsArray,
+//   (bill) => {
+//     const formattedBill = {
+//       prefix: bill.prefix,
+//       number: bill.number.toString(),
+//       name: bill.name,
+//       year: bill.year,
+//       session: bill.session
+//     };
+//     return db.model('bills').create(formattedBill).catch(() => billDupeCounter++)
+// })
 
-// const seedBills = () => db.Promise.map([
-// 	{prefix: 'H', number: '101', session: '115', name: 'Pro-Gun Bill of 2016', year: 2016},
-// 	{prefix: 'H', number: '102', session: '115', name: 'Another Pro-Gun Bill of 2016', year: 2016},
-// 	{prefix: 'H', number: '103', session: '115', name: 'A third Pro-Gun Bill of 2016', year: 2016},
-// 	{prefix: 'H', number: '104', session: '115', name: 'An Anti-Gun Bill of 2016', year: 2015},
-// 	{prefix: 'H', number: '105', session: '115', name: 'A second Anti-Gun Bill of 2016', year: 2016},
-//   {prefix: 'H', number: '105', session: '115', name: 'A duplicate bill', year: 2016},
-//   {prefix: 'H', number: '105', session: '115', name: 'Another duplicate bill', year: 2016}
-// ], bill => db.model('bills').create(bill).catch(() => console.error('You made a duplicate request')))
+const seedBills = () => db.Promise.map([
+	{prefix: 'H', number: '101', session: '115', name: 'Pro-Gun Bill of 2016', year: 2016},
+	{prefix: 'H', number: '102', session: '115', name: 'Another Pro-Gun Bill of 2016', year: 2016},
+	{prefix: 'H', number: '103', session: '115', name: 'A third Pro-Gun Bill of 2016', year: 2016},
+	{prefix: 'H', number: '104', session: '115', name: 'An Anti-Gun Bill of 2016', year: 2015},
+  {prefix: 'H', number: '105', session: '115', name: 'Another duplicate bill', year: 2016}
+], bill => db.model('bills').create(bill).catch(() => console.error('You made a duplicate request')))
 
 
 // const updateBill = () => db.Promise.map([
 //   {prefix: 'H', number: '105', session: '115', name: 'A duplicate bill', year: 2016}
 // ], bill => db.model('bills').update(bill))
 
-const seedMembers = (members) => db.Promise.map(members,
-  (member) => {
-    const formattedMember = {
-      firstName: member.first_name,
-      middleName: member.middle_name,
-      lastName: member.last_name,
-      ppid: member.ppid,
-      party: member.party,
-      chamber: member.chamber,
-      state: member.state,
-      district: member.district,
-      electionYear: member.election_year
-    };
-    return db.model('members').create(formattedMember);
-})
-// const seedMembers = () => db.Promise.map([
-// 	{firstName: 'Rosa', middleName: null, lastName: 'DeLauro', ppid: 'D000216', party: 'D', chamber: 'house', state: 'CT', district: '3', electionYear: '2018'},
-// 	{firstName: 'Lloyd', middleName: null, lastName: 'Doggett', ppid: 'D000399', party: 'D', chamber: 'house', state: 'TX', district: '35', electionYear: '2018'},
-//   {firstName: 'Darth', middleName: null, lastName: 'Vader-Mitchell', ppid: 'Z666999', party: 'I', chamber: 'senate', state: 'NY', district: '69', electionYear: '2020'},
-//   {firstName: 'Luke', middleName: null, lastName: 'SkyWalker', ppid: 'Z666999', party: 'D', chamber: 'senate', state: 'MI', district: '89', electionYear: '2020'},
-//   {firstName: 'Naruto', middleName: null, lastName: 'Johnuzaki', ppid: 'A123456', party: 'I', chamber: 'senate', state: 'NY', district: '69', electionYear: '2020'},
-//   {firstName: 'Eric', middleName: null, lastName: 'Fromm', ppid: 'A183921', party: 'I', chamber: 'senate', state: 'NY', district: '69', electionYear: '2020'},
-//   {firstName: 'Van', middleName: null, lastName: 'Helsing', ppid: 'B183819', party: 'D', chamber: 'senate', state: 'NY', district: '69', electionYear: '2020'},
-//   {firstName: 'Sasuke', middleName: null, lastName: 'Brother', ppid: 'C183839', party: 'D', chamber: 'senate', state: 'NY', district: '69', electionYear: '2020'},
-//   {firstName: 'Bro', middleName: null, lastName: 'Mo-Sapiens', ppid: 'D183912', party: 'I', chamber: 'senate', state: 'NY', district: '69', electionYear: '2020'},
+// const seedMembers = (members) => db.Promise.map(members,
+//   (member) => {
+//     const formattedMember = {
+//       firstName: member.first_name,
+//       middleName: member.middle_name,
+//       lastName: member.last_name,
+//       ppid: member.ppid,
+//       party: member.party,
+//       chamber: member.chamber,
+//       state: member.state,
+//       district: member.district,
+//       electionYear: member.election_year
+//     };
+//     return db.model('members').create(formattedMember);
+// })
+const seedMembers = () => db.Promise.map([
+	{firstName: 'Rosa', middleName: null, lastName: 'DeLauro', ppid: 'D000216', party: 'D', chamber: 'house', state: 'CT', district: '3', electionYear: '2018'},
+	{firstName: 'Lloyd', middleName: null, lastName: 'Doggett', ppid: 'D000399', party: 'D', chamber: 'house', state: 'TX', district: '35', electionYear: '2018'},
+  {firstName: 'Darth', middleName: null, lastName: 'Vader-Mitchell', ppid: 'Z666998', party: 'I', chamber: 'senate', state: 'NY', district: '69', electionYear: '2020'},
+  {firstName: 'Luke', middleName: null, lastName: 'SkyWalker', ppid: 'Z666999', party: 'D', chamber: 'senate', state: 'MI', district: '89', electionYear: '2020'},
+  {firstName: 'Naruto', middleName: null, lastName: 'Johnuzaki', ppid: 'A123456', party: 'I', chamber: 'senate', state: 'NY', district: '69', electionYear: '2020'},
+  {firstName: 'Eric', middleName: null, lastName: 'Fromm', ppid: 'A183921', party: 'I', chamber: 'senate', state: 'NY', district: '69', electionYear: '2020'},
+  {firstName: 'Van', middleName: null, lastName: 'Helsing', ppid: 'B183819', party: 'D', chamber: 'senate', state: 'NY', district: '69', electionYear: '2020'},
+  {firstName: 'Sasuke', middleName: null, lastName: 'Brother', ppid: 'C183839', party: 'D', chamber: 'senate', state: 'NY', district: '69', electionYear: '2020'},
+  {firstName: 'Bro', middleName: null, lastName: 'Mo-Sapiens', ppid: 'D183912', party: 'I', chamber: 'senate', state: 'NY', district: '69', electionYear: '2020'},
 
+], member => db.model('members').create(member).catch(() => console.error('You made a duplicate request')))
+
+// const seedWrongMembers = () => db.Promise.map([
+//   {firstName: 'Ted', middleName: null, lastName: 'Danson', ppid: 'D000217', party: 'I', chamber: 'house', state: 'CO', district: '4', electionYear: '2018'},
+//   {firstName: 'Jake', middleName: 'Fred', lastName: 'Nike', ppid: 'D000399', party: 'R', chamber: 'house', state: 'TN', district: '32', electionYear: '2018'}
 // ], member => db.model('members').create(member))
-
-const seedWrongMembers = () => db.Promise.map([
-  {firstName: 'Ted', middleName: null, lastName: 'Danson', ppid: 'D000217', party: 'I', chamber: 'house', state: 'CO', district: '4', electionYear: '2018'},
-  {firstName: 'Jake', middleName: 'Fred', lastName: 'Nike', ppid: 'D000399', party: 'R', chamber: 'house', state: 'TN', district: '32', electionYear: '2018'}
-], member => db.model('members').create(member))
 
 const seedMembersInfo = (members) => db.Promise.map(members,
   (memberInfo) => {
@@ -99,33 +97,128 @@ const seedMembersInfo = (members) => db.Promise.map(members,
 
 var data;
 
-db.sync({force: true})
-  .then(() => {
-    return allData.getIssues();
-  })
-  .then((issues) => {
-    return seedIssues(issues);
-  })
-  .then(issues => console.log(`Seeded ${issues.length} issues OK`))
-  .then(() => {
-    data = allData.getData();
-    return data;
-  })
-  .then((members) => {
-    data = members;
-    return seedMembers(members);
-  })
-  .then(members => console.log(`Seeded ${members.length} members OK`))
-  .then(() => {
-    let billsArray = [];
-    data.forEach(member => {
-      billsArray = billsArray.concat(member.positions);
-    });
-    return seedBills(billsArray);
-  })
-  .then(bills => console.log(`Seeded ${bills.length - billDupeCounter} bills OK`))
-  .then(seedMembersInfo)
-  .then(memberInfo => console.log(`Seeded ${memberInfo.length} memberInfo OK`))
+db.sync({force: false})
+  // .then(() => {
+  //   return allData.getIssues();
+  // })
+  // .then((issues) => {
+  //   return seedIssues(issues);
+  // })
+  // .then(issues => console.log(`Seeded ${issues.length} issues OK`))
+  // .then(() => {
+  //   data = allData.getData();
+  //   return data;
+  // })
+  // .then((members) => {
+  //   data = members;
+  //   return seedMembers(members);
+  // })
+  // .then(members => console.log(`Seeded ${members.length} members OK`))
+  // .then(() => {
+  //   let billsArray = [];
+  //   data.forEach(member => {
+  //     billsArray = billsArray.concat(member.positions);
+  //   });
+  //   return seedBills(billsArray);
+  // })
+  // .then(bills => console.log(`Seeded ${bills.length - billDupeCounter} bills OK`))
+
+
+
+
+
+//fromhere
+
+
+
+
+//   .then(seedIssues)
+//   .then(issues => console.log(`Seeded ${issues.length} issues OK`))
+//   .then(seedBills)
+//   .then(bills => console.log(`Seeded ${bills.length} bills OK`))
+//   .then(seedMembers)
+//   .then(members => console.log(`Seeded ${members.length} members OK`))
+//   // .then(seedMembersInfo)
+//   // .then(memberInfo => console.log(`Seeded ${memberInfo.length} memberInfo OK`))
+//   .then(seedCats)
+//   .then(cat => console.log(`Seeded ${cat.length} catagories OK`))
+//   .then(() => Cat.findById(1))
+//   // .then((cat) => console.log(cat))
+//   .then((cat) => {cat.addIssue_cat(1, {plusOrMinus: '+'})
+//   return cat})
+//   .then((cat) => cat.addIssue_cat(2, {plusOrMinus: '-'}))
+//   // .then(() => Cat.findById(2))
+//   // .then((cat) => cat.addIssue_cats([1]))
+//   .then(() => Issue.findById(1))
+//   .then((issue) => issue.addIssue_bills(4))
+//   .then(billIssue => billIssue.setDataValue('forOrAgainst', 'for'))
+//   .then(() => Issue.findById(1))
+//   .then((issue) => issue.addIssue_bills(5))
+//   .then(billIssue => billIssue.setDataValue('forOrAgainst', 'against'))
+
+// // .then(([bill, issue]) => bill.addIssue_bill(issue, {forOrAgainst: 'for'}))
+
+  // .then(() => Promise.all([Issue.findById(1), Issue.findById(2)]))
+  // .then((issues) => Promise.all([issues, Bill.findById(5)]))
+  // .then(([issues, bill]) => bill.addIssue_bills(issues))
+
+
+//   .then(() => Member.findById(1))
+//   .then((member) => member.addBill_vote(1, {position: 'yes'}))
+//   .then(() => Member.findById(1))
+//   .then((member) => member.addBill_vote(2, {position: 'no'}))
+//   .then(() => Member.findById(1))
+//   .then((member) => member.addBill_vote(3, {position: 'yes'}))
+//   .then(() => Member.findById(1))
+//   .then((member) => member.addBill_vote(4, {position: 'yes'}))
+//   .then(() => Member.findById(1))
+//   .then((member) => member.addBill_vote(5, {position: 'no'}))
+//   .then(() => Member.findById(2))
+//   .then((member) => member.addBill_vote(1, {position: 'no'}))
+//   .then(() => Member.findById(2))
+//   .then((member) => member.addBill_vote(2, {position: 'no'}))
+//   .then(() => Member.findById(2))
+//   .then((member) => member.addBill_vote(3, {position: 'no'}))
+//   .then(() => Member.findById(2))
+//   .then((member) => member.addBill_vote(4, {position: 'no'}))
+//   .then(() => Member.findById(2))
+//   .then((member) => member.addBill_vote(5, {position: 'no'}))
+//   .then(() => Member.findById(1))
+//   .then(member => member.getIssueScore(1))
+//   .then(score => console.log('Lloyd anti-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
+//   .then(() => Member.findById(1))
+//   .then(member => member.getIssueScore(2))
+//   .then(score => console.log('Lloyd pro-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
+//   .then(() => Member.findById(2))
+//   .then(member => member.getIssueScore(1))
+//   .then(score => console.log('Rosa anti-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
+//   .then(() => Member.findById(2))
+//   .then(member => member.getIssueScore(2))
+//   .then(score => console.log('Rosa pro-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
+//   .then(() => Member.findById(1))
+//   .then(member => member.getIssueScore(1, 2012, 2015))
+//   .then(score => console.log('Lloyd pre-2016 anti-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
+//   .then(() => Member.findById(1))
+//   .then(member => member.getIssueScore(2, 2012, 2015))
+//   .then(score => console.log('Lloyd pre-2016 pro-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
+//   .then(() => Member.findById(1))
+//   .then(member => member.getCatScore(1, 2012, 2016))
+//   .then(score => console.log("Lloyd's array of scores for alignment to possible user positions on Gun Control are:\n", score, '\nIn order, these alignments are if the user\n [strongly disagrees, disagrees, is neutral, agrees, strongly agrees] with the issue.'))
+//   .then(() => Bill.findById(5))
+//   .then(bill => bill.update({name: 'A new name!'}))
+
+
+
+// tohere
+
+
+
+
+  // .then(seedMembersInfo)
+  // .then(memberInfo => console.log(`Seeded ${memberInfo.length} memberInfo OK`))
+  
+
+
   // .then(seedCats)
   // .then(cat => console.log(`Seeded ${cat.length} catagories OK`))
   // .then(() => Cat.findById(1))
@@ -137,10 +230,25 @@ db.sync({force: true})
   // // .then((cat) => cat.addIssue_cats([1]))
   // .then(() => Issue.findById(1))
   // .then((issue) => issue.addIssue_bills([4, 5]))
+  
+
   // .then(() => Issue.findById(2))
   // .then((issue) => issue.addIssue_bills([1, 2, 3]))
+
+  // .then(() => {
+  //   return Promise.all([Bill.findOne({where: {prefix: 's', number: '84', session: '115'}}), Issue.findOne({where: {catCode: 'A3100'}})])
+  // })
+  // .then(([bill, issue]) => bill.addIssue_bill(issue, {forOrAgainst: 'for'}))
+
   // .then(() => Member.findById(1))
-  // .then((member) => member.addBill_vote(1, {position: 'yes'}))
+  // .then((member) => member.addBill_vote(2, {position: 'no'}))
+
+  // .then(() => Member.findById(1))
+  // .then((member) => member.addBill_votes(4, {position: 'yes'}))
+
+
+
+
   // .then(() => Member.findById(1))
   // .then((member) => member.addBill_vote(2, {position: 'no'}))
   // .then(() => Member.findById(1))
@@ -164,25 +272,27 @@ db.sync({force: true})
   // // .then((issue) => issue.getIssue_bills())
   // // .then(billArr => {billArr.forEach(bill => console.log(bill.id));})
 
-  // .then(() => Member.findById(1))
-  // .then(member => member.getIssueScore(1))
-  // .then(score => console.log('Lloyd anti-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
-  // .then(() => Member.findById(1))
-  // .then(member => member.getIssueScore(2))
-  // .then(score => console.log('Lloyd pro-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
-  // .then(() => Member.findById(2))
-  // .then(member => member.getIssueScore(1))
-  // .then(score => console.log('Rosa anti-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
-  // .then(() => Member.findById(2))
-  // .then(member => member.getIssueScore(2))
-  // .then(score => console.log('Rosa pro-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
-  // .then(() => Member.findById(1))
-  // .then(member => member.getIssueScore(1, 2012, 2015))
-  // .then(score => console.log('Lloyd pre-2016 anti-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
-  // .then(() => Member.findById(1))
-  // .then(member => member.getIssueScore(2, 2012, 2015))
-  // .then(score => console.log('Lloyd pre-2016 pro-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
-
+  .then(() => Member.findById(1))
+  .then(member => member.getIssueScore(1))
+  .then(score => console.log('Lloyd anti-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
+  .then(() => Member.findById(1))
+  .then(member => member.getIssueScore(2))
+  .then(score => console.log('Lloyd pro-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
+  .then(() => Member.findById(2))
+  .then(member => member.getIssueScore(1))
+  .then(score => console.log('Rosa anti-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
+  .then(() => Member.findById(2))
+  .then(member => member.getIssueScore(2))
+  .then(score => console.log('Rosa pro-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
+  .then(() => Member.findById(1))
+  .then(member => member.getIssueScore(1, 2012, 2015))
+  .then(score => console.log('Lloyd pre-2016 anti-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
+  .then(() => Member.findById(1))
+  .then(member => member.getIssueScore(2, 2012, 2015))
+  .then(score => console.log('Lloyd pre-2016 pro-gun vote record is:', score[0], 'yes votes out of', score[1], 'total votes'))
+  .then(() => Member.findById(1))
+  .then(member => member.getCatScore(1, 2012, 2016))
+  .then(catScore => console.log('Lloyd Gun Control Cat Score is: ', catScore))
   // .then(() => Member.findById(1))
   // .then(member => member.getCatScore(1, 2012, 2016))
   // .then(score => console.log("Lloyd's array of scores for alignment to possible user positions on Gun Control are:\n", score, '\nIn order, these alignments are if the user\n [strongly disagrees, disagrees, is neutral, agrees, strongly agrees] with the issue.'))
